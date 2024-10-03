e8897b0acfe8418fd1d316b4721a04d028ad70bc
Tanny0209 <tanmay.s237554101@vcet.edu.in>
2024-10-03 21:14:10 +0530

    All Crops Backend

--- Server/models/AllCrops.js
+++ Server/models/AllCrops.js
@@ -1,7 +1,7 @@
 // server/models/Crop.js
 const mongoose = require('mongoose');
 
-const cropSchema = new mongoose.Schema({
+const AllCropSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   growingTechnique: { type: String },
@@ -15,5 +15,5 @@ const cropSchema = new mongoose.Schema({
   harvesting: { type: String },
 });
 
-const AllCrops = mongoose.model('AllCrops', cropSchema);
+const AllCrops = mongoose.model('AllCrops', AllCropSchema);
 module.exports = AllCrops;
